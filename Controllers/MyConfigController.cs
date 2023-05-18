a214a22d77c4b73e320b76557303c2acbcae7301
beachscout24 <eric.w.clarke@gmail.com>
2023-05-18 12:11:00 -0400

    Initial Ckeck in

---
+++ Controllers/MyConfigController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace configurationSource.Controllers
+{
+	[Route("[controller]")]
+	[ApiController]
+	public class MyConfigController : ControllerBase
+	{
+		private readonly IConfiguration _configuration;
+        public MyConfigController(IConfiguration configuration)
+        {
+			_configuration = configuration;
+        }
+
+        [HttpGet]
+		public Dictionary<string, string?> Get()
+		{
+			return _configuration.GetSection("Demo").GetChildren().ToDictionary(a => a.Key, a => a.Value);
+		}
+	}
+}
